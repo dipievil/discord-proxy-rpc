c603fcdaba4b54343af57ea4ccb9929e32907a61
dipi.evil <dipi.evil@gmail.com>
2026-09-10 14:56:20 -0300

    fix(config): use manual PROXY_TOKEN override to ensure priority over PROXY_AUTH_TOKEN

--- internal/config/config.go
+++ internal/config/config.go
@@ -100,7 +100,11 @@ func setupEnv(v *viper.Viper) {
 	_ = v.BindEnv("server.write_timeout", "PROXY_SERVER_WRITE_TIMEOUT")
 
 	_ = v.BindEnv("auth.enabled", "PROXY_AUTH_ENABLED")
-	_ = v.BindEnv("auth.token", "PROXY_TOKEN", "PROXY_AUTH_TOKEN")
+	_ = v.BindEnv("auth.token", "PROXY_AUTH_TOKEN")
+
+	if token := os.Getenv("PROXY_TOKEN"); token != "" {
+		v.Set("auth.token", token)
+	}
 
 	_ = v.BindEnv("mdns.enabled", "PROXY_MDNS_ENABLED")
 	_ = v.BindEnv("mdns.instance_name", "PROXY_MDNS_INSTANCE_NAME")
